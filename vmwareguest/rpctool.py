54cf0ad58a980c5e2d524d11b7193e43e8f688b6
Adrian Vladu <avladu@cloudbasesolutions.com>
2020-01-29 23:25:21 +0200

    Added rpctool encoding return

--- vmwareguest/rpctool.py
+++ vmwareguest/rpctool.py
@@ -8,7 +8,7 @@ def main():
     parser.add_argument("command", default="info-get guestinfo.metadata",
                         help="VMware key and namespace to retrieve")
     args = parser.parse_args()
-
+    encode_b64 = False
     if args.command == "info-get guestinfo.metadata":
         data = json.dumps({
             "local-hostname": "dummy-hostname",
@@ -17,12 +17,21 @@ def main():
             "admin-password": "Passw0rd"
         })
         data = data.encode("utf-8")
+        encode_b64 = True
 
     if args.command == "info-get guestinfo.userdata":
         data = gzip.compress(b'#ps1\nmkdir C:\\test')
+        encode_b64 = True
 
-    str_out = base64.b64encode(data)
-    print(str_out.decode(), end='')
+    if args.command == "info-get guestinfo.userdata.encoding":
+        data = 'gz+b64'
+    if args.command == "info-get guestinfo.metadata.encoding":
+        data = 'b64'
+
+    if encode_b64:
+        data = base64.b64encode(data).decode()
+
+    print(data, end='')
 
 
 if __name__ == "__main__":
